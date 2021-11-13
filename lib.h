c181d71d00e64718cab0f9be0ee0de8c23793606
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2021-11-13 22:51:29 +0100

    Commit #05 [Moved from tabs to 2 spaces, added split functions]

--- lib.h
+++ lib.h
@@ -1,163 +1,164 @@
 #ifndef _CSTRING_H_
 #define _CSTRING_H_
 
-#define CVECTOR_POINTERMODE
-#include <c-vector/lib.h>
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-CVECTOR_WITH_NAME(char, StringBuffer);
 typedef char* String;
+#define CVECTOR_POINTERMODE
+#include <c-vector/lib.h>
+CVECTOR_WITH_NAME(char, StringBuffer);
+CVECTOR(String);
 typedef struct FixedString FixedString;
 
 __cvector_inline__
 CVECTOR_STATUS StringBuffer_push_bytes(StringBuffer *sb, char *str,
-										int n) {
-	for (int i = 0; i < n; i++) {
+                                       int n) {
+  for (int i = 0; i < n; i++) {
 #ifdef CVECTOR_SILENT
-		if (StringBuffer_push(sb, str[i]))
-			return 1;
+    if (StringBuffer_push(sb, str[i]))
+      return 1;
 #else
-		StringBuffer_push(sb, str[i]);
+    StringBuffer_push(sb, str[i]);
 #endif
-	}
-	CVECTOR_RETURN(0);
+  }
+  CVECTOR_RETURN(0);
 }
 
 __cvector_inline__
 CVECTOR_STATUS StringBuffer_push_str(StringBuffer *sb, char *str) {
-	CVECTOR_RETURN(StringBuffer_push_bytes(sb, str, strlen(str)));
+  CVECTOR_RETURN(StringBuffer_push_bytes(sb, str, strlen(str)));
 }
 
 __cvector_inline__
 String String_new() {
-	return StringBuffer_with_length(1);
+  return StringBuffer_with_length(1);
 }
 
 __cvector_inline__
 String String_from_bytes(char *bytes, int n) {
-	String str = StringBuffer_with_length(n + 1);
-	if (str == NULL)
-		return NULL;
-	memcpy(str, bytes, n);
-	str[n] = '\0';
-	return str;
+  String str = StringBuffer_with_length(n + 1);
+  if (str == NULL)
+    return NULL;
+  memcpy(str, bytes, n);
+  str[n] = '\0';
+  return str;
 }
 
 __cvector_inline__
 String String_from(char *str) {
-	return String_from_bytes(str, strlen(str));
+  return String_from_bytes(str, strlen(str));
 }
 
 __cvector_inline__
 int String_len(String str) {
-	return StringBuffer_len(str) - 1;
+  return StringBuffer_len(str) - 1;
 }
 
 __cvector_inline__
 String String_clone(String str) {
-	return String_from_bytes(str, String_len(str));
+  return String_from_bytes(str, String_len(str));
 }
 
 __cvector_inline__
 String String_concat(String str1, String str2) {
-	int len1 = String_len(str1);
-	int len2 = String_len(str2);
-	String str = StringBuffer_with_length(len1 + len2 + 1);
-	if (str == NULL)
-		return NULL;
-	memcpy(str, str1, len1);
-	memcpy(str + len1, str2, len2);
-	str[len1 + len2] = '\0';
-	return str;
+  int len1 = String_len(str1);
+  int len2 = String_len(str2);
+  String str = StringBuffer_with_length(len1 + len2 + 1);
+  if (str == NULL)
+    return NULL;
+  memcpy(str, str1, len1);
+  memcpy(str + len1, str2, len2);
+  str[len1 + len2] = '\0';
+  return str;
 }
 
 static
 uint32_t String_hash(String str) {
-	int len = String_len(str);
+  int len = String_len(str);
 
-	const size_t m = 0x5bd1e995;
-	const int r = 24;
+  const size_t m = 0x5bd1e995;
+  const int r = 24;
 
-	size_t h = len ^ 0;
+  size_t h = len ^ 0;
 
-	while(len >= 4)
-	{
-		size_t k = *str;
+  while(len >= 4)
+  {
+    size_t k = *str;
 
-		k *= m; 
-		k ^= k >> r; 
-		k *= m; 
-		
-		h *= m; 
-		h ^= k;
+    k *= m; 
+    k ^= k >> r; 
+    k *= m; 
+    
+    h *= m; 
+    h ^= k;
 
-		str += 4;
-		len -= 4;
-	}
-	
-	// Handle the last few bytes of the input array
+    str += 4;
+    len -= 4;
+  }
+  
+  // Handle the last few bytes of the input array
 
-	switch(len)
-	{
-	case 3: h ^= str[2] << 16;
-	case 2: h ^= str[1] << 8;
-	case 1: h ^= str[0];
-	        h *= m;
-	};
+  switch(len)
+  {
+  case 3: h ^= str[2] << 16;
+  case 2: h ^= str[1] << 8;
+  case 1: h ^= str[0];
+          h *= m;
+  };
 
-	// Do a few final mixes of the hash to ensure the last few
-	// bytes are well-incorporated.
+  // Do a few final mixes of the hash to ensure the last few
+  // bytes are well-incorporated.
 
-	h ^= h >> 13;
-	h *= m;
-	h ^= h >> 15;
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
 
-	return h;
+  return h;
 
 }
 
 __cvector_inline__
 String String_slice(String str, signed int fi, signed int li) {
-	if (fi < 0)
-		fi += String_len(str) + 1;
-	if (li < 0)
-		li += String_len(str) + 1;
-	if (fi < 0 || fi > String_len(str) || li < fi || li < 0 ||
-			li > String_len(str))
-		return NULL;
-	int len = li - fi;
-	String slice = StringBuffer_with_length(len + 1);
-	if (slice == NULL)
-		return NULL;
-	memcpy(slice, str + fi, len);
-	return slice;
+  if (fi < 0)
+    fi += String_len(str) + 1;
+  if (li < 0)
+    li += String_len(str) + 1;
+  if (fi < 0 || fi > String_len(str) || li < fi || li < 0 ||
+      li > String_len(str))
+    return NULL;
+  int len = li - fi;
+  String slice = StringBuffer_with_length(len + 1);
+  if (slice == NULL)
+    return NULL;
+  memcpy(slice, str + fi, len);
+  return slice;
 }
 
 __cvector_inline__
 CVECTOR_STATUS String_append(String *str, char *bstr) {
-	cvector_get_header(*str)->len -= 1;
+  cvector_get_header(*str)->len -= 1;
 #ifdef CVECTOR_SILENT
-	StringBuffer_push_str(str, bstr);
-	StringBuffer_push(str, '\0');
+  StringBuffer_push_str(str, bstr);
+  StringBuffer_push(str, '\0');
 #else
-	if (StringBuffer_push_str(str, bstr))
-	{
-		CVECTOR_RETURN(1);
-	}
-	if (StringBuffer_push(str, '\0'))
-	{
-		CVECTOR_RETURN(1);
-	}
-	CVECTOR_RETURN(0);
+  if (StringBuffer_push_str(str, bstr))
+  {
+    CVECTOR_RETURN(1);
+  }
+  if (StringBuffer_push(str, '\0'))
+  {
+    CVECTOR_RETURN(1);
+  }
+  CVECTOR_RETURN(0);
 #endif
 }
 
 __cvector_inline__
 bool String_equal(String str1, String str2) {
-	return String_len(str1) == String_len(str2) &&
-			!memcmp(str1, str2, String_len(str1));
+  return String_len(str1) == String_len(str2) &&
+      !memcmp(str1, str2, String_len(str1));
 }
 
 __cvector_inline__
@@ -166,40 +167,79 @@ void String_cleanup(String str) { StringBuffer_cleanup(str); }
 
 __cvector_inline__
 String StringBuffer_to_string(StringBuffer sb) {
-	return String_from_bytes(sb, StringBuffer_len(sb));
+  return String_from_bytes(sb, StringBuffer_len(sb));
 }
 
 __cvector_inline__
 String StringBuffer_transform_to_string(StringBuffer *sb) {
-	StringBuffer_push(sb, '\0');
-	return *sb;
+  StringBuffer_push(sb, '\0');
+  return *sb;
+}
+
+static Vector_String String_split(String str, char *p) {
+  Vector_String vec = Vector_String_new();
+  int p_len = strlen(p);
+  char *end = str + StringBuffer_len(str);
+  char *start = str;
+  char *ptr = str;
+  for (; ptr < end;) {
+    if (memcmp(ptr, p, p_len) == 0) {
+      int s_len = ptr - start;
+      Vector_String_push(&vec, String_from_bytes(start, s_len));
+      ptr += p_len;
+      start = ptr;
+    } else {
+      ++ptr;
+    }
+  }
+  Vector_String_push(&vec, String_from_bytes(start, end - start));
+  return vec;
+}
+
+static Vector_String String_split_by_char(String str, char c)
+{
+  Vector_String vec = Vector_String_new();
+  char *end = str + StringBuffer_len(str);
+  char *start = str;
+  char *ptr = str;
+  for (; ptr < end;) {
+    if (*ptr == c) {
+      int s_len = ptr - start;
+      Vector_String_push(&vec, String_from_bytes(start, s_len));
+      start = ++ptr;
+    } else {
+      ++ptr;
+    }
+  }
+  Vector_String_push(&vec, String_from_bytes(start, end - start));
+  return vec;
 }
 
 struct FixedString {
-	int        len;
-	const char *str;
-	bool       literal;
+  int        len;
+  const char *str;
+  bool       literal;
 };
 
 #define FixedString_from(str) ((FixedString){ sizeof(str) - 1, str, 1 })
 
 __cvector_inline__
 FixedString String_to_fixed(String str) {
-	int len = String_len(str);
-	char *data = malloc(len + 1);
-	memcpy(str, data, len + 1);
-	return (FixedString){ len, (char const *)data, 0 };
+  int len = String_len(str);
+  char *data = malloc(len + 1);
+  memcpy(str, data, len + 1);
+  return (FixedString){ len, (char const *)data, 0 };
 }
 #define FixedString_char_at(str, i) (i < (str).len ? (str).str[i] : -1)
 __cvector_inline__
 void FixedString_cleanup(FixedString *fstr) {
-	if (fstr != NULL)
-	{
-		if (fstr->str != NULL && !fstr->literal) free((void *)fstr->str);
-		fstr->len = 0;
-		fstr->str = NULL;
-		fstr->literal = 0;
-	}
+  if (fstr != NULL)
+  {
+    if (fstr->str != NULL && !fstr->literal) free((void *)fstr->str);
+    fstr->len = 0;
+    fstr->str = NULL;
+    fstr->literal = 0;
+  }
 }
 
 #endif /* _CSTRING_H_ */
--- test/cstring_test.c
+++ test/cstring_test.c
@@ -2,149 +2,185 @@
 #include "testing.h"
 
 void test_String_new(void) {
-	String str = String_new();
-	CU_ASSERT_PTR_NOT_NULL_FATAL(str);
-	CU_ASSERT_EQUAL_FATAL(String_len(str), 0);
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 1);
-	String_cleanup(str);
+  String str = String_new();
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  CU_ASSERT_EQUAL_FATAL(String_len(str), 0);
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 1);
+  String_cleanup(str);
 }
 void test_String_from_bytes(void) {
-	String str = String_from_bytes("ab\0cd\x1b", 6);
-	CU_ASSERT_PTR_NOT_NULL_FATAL(str);
-	CU_ASSERT_EQUAL_FATAL(String_len(str), 6);
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 7);
-	String_cleanup(str);
+  String str = String_from_bytes("ab\0cd\x1b", 6);
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  CU_ASSERT_EQUAL_FATAL(String_len(str), 6);
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 7);
+  String_cleanup(str);
 }
 void test_String_from(void) {
-	String str = String_from("random string");
-	CU_ASSERT_PTR_NOT_NULL_FATAL(str);
-	CU_ASSERT_EQUAL_FATAL(String_len(str), 13);
-	CU_ASSERT_EQUAL_FATAL(strlen(str), 13);
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 14);
-	String_cleanup(str);
+  String str = String_from("random string");
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  CU_ASSERT_EQUAL_FATAL(String_len(str), 13);
+  CU_ASSERT_EQUAL_FATAL(strlen(str), 13);
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_len(str), 14);
+  String_cleanup(str);
 }
 void test_String_clone(void) {
-	String str1 = String_from("another random string");
-	String str2 = String_clone(str1);
-	CU_ASSERT_STRING_EQUAL_FATAL(str1, str2);
-	String_cleanup(str1);
-	String_cleanup(str2);
+  String str1 = String_from("another random string");
+  String str2 = String_clone(str1);
+  CU_ASSERT_STRING_EQUAL_FATAL(str1, str2);
+  String_cleanup(str1);
+  String_cleanup(str2);
 }
 void test_String_concat(void) {
-	String str1 = String_from("foo ");
-	String str2 = String_from("bar");
-	String str = String_concat(str1, str2);
-	CU_ASSERT_EQUAL_FATAL(String_len(str),
-							String_len(str1) + String_len(str2));
-	CU_ASSERT_STRING_EQUAL_FATAL(str, "foo bar");
+  String str1 = String_from("foo ");
+  String str2 = String_from("bar");
+  String str = String_concat(str1, str2);
+  CU_ASSERT_EQUAL_FATAL(String_len(str),
+              String_len(str1) + String_len(str2));
+  CU_ASSERT_STRING_EQUAL_FATAL(str, "foo bar");
 }
 void test_String_slice(void) {
-	String str = String_from("out of ideas");
-	CU_ASSERT_PTR_NOT_NULL_FATAL(str);
-	String slice1 = String_slice(str, 0, 3);
-	CU_ASSERT_STRING_EQUAL_FATAL(slice1, "out");
-	String slice2 = String_slice(str, 4, -1);
-	CU_ASSERT_STRING_EQUAL_FATAL(slice2, "of ideas");
-	String slice3 = String_slice(str, -6, -1);
-	CU_ASSERT_STRING_EQUAL_FATAL(slice3, "ideas");
-	String slice4 = String_slice(str, -9, 6);
-	CU_ASSERT_STRING_EQUAL_FATAL(slice4, "of");
-	String_cleanup(str);
-	String_cleanup(slice1);
-	String_cleanup(slice2);
-	String_cleanup(slice3);
-	String_cleanup(slice4);
+  String str = String_from("out of ideas");
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  String slice1 = String_slice(str, 0, 3);
+  CU_ASSERT_STRING_EQUAL_FATAL(slice1, "out");
+  String slice2 = String_slice(str, 4, -1);
+  CU_ASSERT_STRING_EQUAL_FATAL(slice2, "of ideas");
+  String slice3 = String_slice(str, -6, -1);
+  CU_ASSERT_STRING_EQUAL_FATAL(slice3, "ideas");
+  String slice4 = String_slice(str, -9, 6);
+  CU_ASSERT_STRING_EQUAL_FATAL(slice4, "of");
+  String_cleanup(str);
+  String_cleanup(slice1);
+  String_cleanup(slice2);
+  String_cleanup(slice3);
+  String_cleanup(slice4);
 }
 void test_String_equal(void) {
-	String str1 = String_from("foo");
-	String str2 = String_clone(str1);
-	String str3 = String_from("bar");
-	CU_ASSERT_FATAL(String_equal(str1, str2));
-	CU_ASSERT_FATAL(!String_equal(str1, str3));
+  String str1 = String_from("foo");
+  String str2 = String_clone(str1);
+  String str3 = String_from("bar");
+  CU_ASSERT_FATAL(String_equal(str1, str2));
+  CU_ASSERT_FATAL(!String_equal(str1, str3));
 }
 void test_String_to_fixed(void) {
-	String str = String_from("null\0 byte between");
-	FixedString fstr = String_to_fixed(str);
-	CU_ASSERT_EQUAL_FATAL(String_len(str), fstr.len);
-	CU_ASSERT_EQUAL_FATAL(fstr.literal, 0);
-	CU_ASSERT_FATAL(!memcmp(str, fstr.str, fstr.len));
-	String_cleanup(str);
-	FixedString_cleanup(&fstr);
-	CU_ASSERT_EQUAL_FATAL(fstr.len, 0);
-	CU_ASSERT_EQUAL_FATAL(fstr.literal, 0);
-	CU_ASSERT_PTR_NULL_FATAL(fstr.str);
+  String str = String_from("null\0 byte between");
+  FixedString fstr = String_to_fixed(str);
+  CU_ASSERT_EQUAL_FATAL(String_len(str), fstr.len);
+  CU_ASSERT_EQUAL_FATAL(fstr.literal, 0);
+  CU_ASSERT_FATAL(!memcmp(str, fstr.str, fstr.len));
+  String_cleanup(str);
+  FixedString_cleanup(&fstr);
+  CU_ASSERT_EQUAL_FATAL(fstr.len, 0);
+  CU_ASSERT_EQUAL_FATAL(fstr.literal, 0);
+  CU_ASSERT_PTR_NULL_FATAL(fstr.str);
+}
+
+void test_String_split(void) {
+  String str = String_from("foo 1 bar 1 baz");
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  Vector_String vec = String_split(str, " 1 ");
+  CU_ASSERT_PTR_NOT_NULL_FATAL(vec);
+  CU_ASSERT_EQUAL_FATAL(Vector_String_len(vec), 3);
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[0], "foo");
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[1], "bar");
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[2], "baz");
+  for (int i = 0; i < Vector_String_len(vec); ++i) {
+    String_cleanup(vec[i]);
+  }
+  String_cleanup(str);
+  Vector_String_cleanup(vec);
+}
+
+void test_String_split_by_char(void) {
+  String str = String_from("some_text_with_underscores");
+  CU_ASSERT_PTR_NOT_NULL_FATAL(str);
+  Vector_String vec = String_split_by_char(str, '_');
+  CU_ASSERT_PTR_NOT_NULL_FATAL(vec);
+  printf("len=%d\n", Vector_String_len(vec));
+  CU_ASSERT_EQUAL_FATAL(Vector_String_len(vec), 4);
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[0], "some");
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[1], "text");
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[2], "with");
+  CU_ASSERT_STRING_EQUAL_FATAL(vec[3], "underscores");
+  for (int i = 0; i < Vector_String_len(vec); ++i) {
+    String_cleanup(vec[i]);
+  }
+  String_cleanup(str);
+  Vector_String_cleanup(vec);
 }
 
 void test_StringBuffer_push_bytes(void) {
-	StringBuffer sb = StringBuffer_new();
-	CU_ASSERT_PTR_NOT_NULL_FATAL(sb);
-	CU_ASSERT_EQUAL_FATAL(
-			StringBuffer_push_bytes(&sb, "abcdef\0\x34", 8), 0);
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_len(sb), 8);
-	CU_ASSERT_FATAL(!memcmp(sb, "abcdef\0\x34", 8));
-	StringBuffer_cleanup(sb);
+  StringBuffer sb = StringBuffer_new();
+  CU_ASSERT_PTR_NOT_NULL_FATAL(sb);
+  CU_ASSERT_EQUAL_FATAL(
+      StringBuffer_push_bytes(&sb, "abcdef\0\x34", 8), 0);
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_len(sb), 8);
+  CU_ASSERT_FATAL(!memcmp(sb, "abcdef\0\x34", 8));
+  StringBuffer_cleanup(sb);
 }
 void test_StringBuffer_push_str(void) {
-	StringBuffer sb = StringBuffer_new();
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_push_str(&sb, "lejs me limon"), 0);
-	CU_ASSERT_EQUAL_FATAL(StringBuffer_len(sb), 13);
-	CU_ASSERT_FATAL(!memcmp(sb, "lejs me limon", 13));
+  StringBuffer sb = StringBuffer_new();
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_push_str(&sb, "lejs me limon"), 0);
+  CU_ASSERT_EQUAL_FATAL(StringBuffer_len(sb), 13);
+  CU_ASSERT_FATAL(!memcmp(sb, "lejs me limon", 13));
 }
 void test_FixedString_from(void) {
-	FixedString str = FixedString_from("hello");
-	CU_ASSERT_EQUAL_FATAL(str.len, 5);
-	CU_ASSERT_FATAL(str.literal);
-	CU_ASSERT_FATAL(!strcmp(str.str, "hello"));
-	FixedString_cleanup(&str);
-	CU_ASSERT_EQUAL_FATAL(str.len, 0);
-	CU_ASSERT_FATAL(!str.literal);
-	CU_ASSERT_PTR_NULL_FATAL(str.str);
-	str = FixedString_from("foo\0bar");
-	CU_ASSERT_EQUAL_FATAL(str.len, 7);
-	CU_ASSERT_FATAL(str.literal);
-	CU_ASSERT_FATAL(!memcmp(str.str, "foo\0bar", str.len));
-	FixedString_cleanup(&str);
+  FixedString str = FixedString_from("hello");
+  CU_ASSERT_EQUAL_FATAL(str.len, 5);
+  CU_ASSERT_FATAL(str.literal);
+  CU_ASSERT_FATAL(!strcmp(str.str, "hello"));
+  FixedString_cleanup(&str);
+  CU_ASSERT_EQUAL_FATAL(str.len, 0);
+  CU_ASSERT_FATAL(!str.literal);
+  CU_ASSERT_PTR_NULL_FATAL(str.str);
+  str = FixedString_from("foo\0bar");
+  CU_ASSERT_EQUAL_FATAL(str.len, 7);
+  CU_ASSERT_FATAL(str.literal);
+  CU_ASSERT_FATAL(!memcmp(str.str, "foo\0bar", str.len));
+  FixedString_cleanup(&str);
 }
 void test_FixedString_char_at(void) {
-	FixedString str = FixedString_from("hmmm");
-	CU_ASSERT_EQUAL_FATAL(FixedString_char_at(str, 0), 'h');
-	CU_ASSERT_EQUAL_FATAL(FixedString_char_at(str, str.len), -1);
+  FixedString str = FixedString_from("hmmm");
+  CU_ASSERT_EQUAL_FATAL(FixedString_char_at(str, 0), 'h');
+  CU_ASSERT_EQUAL_FATAL(FixedString_char_at(str, str.len), -1);
 }
 
 int main(int argc, char **argv) {
-	int status = 0;
-	CU_TestInfo String_tests[] = {
-		{ "new",        test_String_new        },
-		{ "from_bytes", test_String_from_bytes },
-		{ "from",       test_String_from       },
-		{ "clone",      test_String_clone	   },
-		{ "concat",     test_String_concat     },
-		{ "slice",      test_String_slice      },
-		{ "equal",      test_String_equal      },
-		{ "to_fixed",   test_String_to_fixed   },
-		CU_TEST_INFO_NULL
-	};
-	CU_TestInfo StringBuffer_tests[] = {
-		{ "push_bytes", test_StringBuffer_push_bytes },
-		{ "push_str",   test_StringBuffer_push_str   },
-		CU_TEST_INFO_NULL
-	};
-	CU_TestInfo FixedString_tests[] = {
-		{ "from",    test_FixedString_from    },
-		{ "char_at", test_FixedString_char_at },
-		CU_TEST_INFO_NULL
-	};
-	CU_SuiteInfo suites[] = {
-		{ "String",       NULL, NULL, NULL, NULL, String_tests       },
-		{ "StringBuffer", NULL, NULL, NULL, NULL, StringBuffer_tests },
-		{ "FixedString", NULL, NULL, NULL, NULL, FixedString_tests },
-		CU_SUITE_INFO_NULL
-	};
-	CU_initialize_registry();
-	if ((status = CU_register_suites(suites)) != CUE_SUCCESS)
-		goto cleanup;
-	RUN_TESTS;
+  int status = 0;
+  CU_TestInfo String_tests[] = {
+    { "new",       test_String_new               },
+    { "from_bytes",     test_String_from_bytes   },
+    { "from",       test_String_from             },
+    { "clone",       test_String_clone           },
+    { "concat",       test_String_concat         },
+    { "slice",       test_String_slice           },
+    { "equal",       test_String_equal           },
+    { "to_fixed",     test_String_to_fixed       },
+    { "split",       test_String_split           },
+    { "split_by_char", test_String_split_by_char },
+    CU_TEST_INFO_NULL
+  };
+  CU_TestInfo StringBuffer_tests[] = {
+    { "push_bytes", test_StringBuffer_push_bytes },
+    { "push_str",   test_StringBuffer_push_str   },
+    CU_TEST_INFO_NULL
+  };
+  CU_TestInfo FixedString_tests[] = {
+    { "from",    test_FixedString_from    },
+    { "char_at", test_FixedString_char_at },
+    CU_TEST_INFO_NULL
+  };
+  CU_SuiteInfo suites[] = {
+    { "String",       NULL, NULL, NULL, NULL, String_tests       },
+    { "StringBuffer", NULL, NULL, NULL, NULL, StringBuffer_tests },
+    { "FixedString", NULL, NULL, NULL, NULL, FixedString_tests },
+    CU_SUITE_INFO_NULL
+  };
+  CU_initialize_registry();
+  if ((status = CU_register_suites(suites)) != CUE_SUCCESS)
+    goto cleanup;
+  RUN_TESTS;
 cleanup:
-	CU_cleanup_registry();
-	return status;
+  CU_cleanup_registry();
+  return status;
 }
